0438239b910c671073b15161af6886580dcdec5a
David Park <dpark4657@gmail.com>
2022-09-24 22:08:19 -0700

    Initial commit

---
+++ arithmetic.rb
@@ -0,0 +1,5 @@
+# dividing integers gives us an integer result
+p 12 / 5
+# need to use floating point numbers if we want floating point results
+p 12.0 / 5.0
+p 2 ** 5 # exponent -> 32``
---
+++ printing.rb
@@ -0,0 +1,19 @@
+# 3 ways to print in Ruby
+puts "Hello world"
+puts "I'm alive and well!"
+puts 3.14159
+puts # this is a line break
+puts 5+9
+
+# print has no default line break
+print "printing"
+print "second print"
+print "\n"
+
+# p method gives a more literal value
+puts "Steven Seagal"
+p "Steven Seagal" # outputs "Steven Seagal"
+puts "Hi there, this is a 
+big line break"
+p "Hi there, this is a 
+big line break"
